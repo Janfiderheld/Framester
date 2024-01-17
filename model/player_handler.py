2069dddddb6ff79235e0d897e523def8cac3c289
Janfiderheld <Janfitoe@web.de>
2024-01-17 14:10:19 +0100

    add handler for player management

---
+++ model/player_handler.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from model import Player, SingletonMeta
+
+
+# Singleton
+class PlayerHandler(metaclass=SingletonMeta):
+    def __init__(self):
+        self.__players: List[Player] = []
+        self.__player_idx = 0
+        self.add_new_player(Player("Test"))
+
+    def add_new_player(self, p: Player):
+        self.__players.append(p)
+
+    def return_current_player(self) -> Player:
+        return self.__players[self.__player_idx]
+
+    def goto_next_player(self):
+        if self.__player_idx == (len(self.__players)-1):
+            self.__player_idx = 0
+        else:
+            self.__player_idx += 1
