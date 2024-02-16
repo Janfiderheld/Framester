47fe39a19e7348b85c6264e462830a68b47ee3b6
Janfiderheld <Janfitoe@web.de>
2024-02-16 23:56:55 +0100

    fix issue #1 and add according unit test

--- model/player.py
+++ model/player.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 from model import Movie
@@ -6,10 +7,11 @@ from model import Movie
 class Player:
     max_points = 8
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, add_start_movie=True):
         self.__name = name
         self.__timeline: List[Movie] = []
-        self.get_starting_movie()
+        if add_start_movie:
+            self.get_starting_movie()
 
     def get_name(self) -> str:
         return self.__name
@@ -32,20 +34,28 @@ class Player:
     def check_correct_pos_in_timeline(self, m: Movie, pos: int) -> bool:
         assert pos <= len(self.__timeline)
 
-        corr_p = 0
-        two_slots = False
+        if len(self.__timeline) == 0:
+            return True
+
+        first = sys.maxsize
+        last = -1
         for t in self.__timeline:
+            idx = self.__timeline.index(t)
+            # Case 1: t is older
             if t.get_year() < m.get_year():
-                corr_p += 1
-            elif t.get_year() == m.get_year():
-                two_slots = True
-                break
-            else:
-                break
-        if two_slots:
-            return pos == corr_p or pos == corr_p+1
-        else:
-            return pos == corr_p
+                first = idx + 1
+                last = idx + 1
+                continue
+            # Case 2: both movies are from the same year
+            if t.get_year() == m.get_year():
+                first = min(idx, first)
+                last = max(idx+1, last)
+                continue
+            # Case 3: new movie is older
+            if m.get_year() < t.get_year():
+                first = idx
+                last = idx
+        return first <= pos <= last
 
     def add_to_timeline(self, m: Movie, pos: int):
         assert pos <= len(self.__timeline)
--- test/timeline_tester.py
+++ test/timeline_tester.py
@@ -29,13 +29,36 @@ class TimelineTest(unittest.TestCase):
         self.assertFalse(result)
 
     def test_add_movie_with_same_year(self):
-        p = Player("TestPlayer")
+        p = Player("TestPlayer", False)
         mo = Movie("test", "-", "", 1800, "")
-        mo2 = Movie("test3", "-", "", 1800, "")
+        mo2 = Movie("test2", "-", "", 1800, "")
         p.add_to_timeline(mo, 0)
         result0 = p.check_correct_pos_in_timeline(mo2, 0)
         result1 = p.check_correct_pos_in_timeline(mo2, 1)
         self.assertEqual(result0, result1)
+
+    def test_add_multiple_movie_with_same_year(self):
+        p = Player("TestPlayer", False)
+        mo = Movie("test1", "-", "", 2023, "")
+        mo2 = Movie("test2", "-", "", 2023, "")
+        mo3 = Movie("test3", "-", "", 2023, "")
+        p.add_to_timeline(mo, 0)
+        p.add_to_timeline(mo2, 0)
+        result0 = p.check_correct_pos_in_timeline(mo3, 0)
+        result1 = p.check_correct_pos_in_timeline(mo3, 1)
+        result2 = p.check_correct_pos_in_timeline(mo3, 2)
+        self.assertTrue(result0)
+        self.assertTrue(result1)
+        self.assertTrue(result2)
+
+    def test_add_movie_with_same_year_at_end(self):
+        p = Player("TestPlayer", False)
+        for i in range(0, p.max_points-1):
+            m = Movie(f"test{i}", "-", "", 2023, "")
+            p.add_to_timeline(m, i)
+        last_m = Movie("last_test", "-", "", 2023, "")
+        result = p.check_correct_pos_in_timeline(last_m, p.max_points-1)
+        self.assertTrue(result)
 
 
 if __name__ == '__main__':
