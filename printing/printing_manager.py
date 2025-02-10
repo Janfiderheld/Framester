2bd923d46dcd26fb5e170a0c04dd269472b15f70
Janfiderheld <Janfitoe@web.de>
2025-02-10 18:44:14 +0100

    creation of backside for each movie card

--- printing/printing_manager.py
+++ printing/printing_manager.py
@@ -1,8 +1,10 @@
 import os
+import random
 import time
+import textwrap
 
 import requests
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 from model import Movie
 
@@ -58,8 +60,56 @@ def crop_and_resize_image(temp_path: str, resulting_path: str, target_size=(1122
         img_cropped.save(resulting_path)
 
 
-def create_back_image(number: int, mov: Movie):
-    pass
+def get_random_color() -> (int, int, int):
+    return random.randint(50, 240), random.randint(50, 240), random.randint(50, 240)
+
+
+def draw_wrapped_text(draw: ImageDraw, text: str, position: (int, int), font: ImageFont, fill="black"):
+    lines = textwrap.wrap(text, width=45)
+    y_offset = 0
+    for line in lines:
+        text_size = draw.textbbox((0, 0), line, font=font)
+        draw.text((position[0], position[1] + y_offset), line, font=font, fill=fill, anchor="mm")
+        y_offset += text_size[3] - text_size[1] + 5
+
+
+def create_back_image(number: int, mov: Movie, target_size=(1122, 822)):
+    bg_color = get_random_color()
+    img = Image.new("RGB", target_size, bg_color)
+    draw = ImageDraw.Draw(img)
+
+    # Set the text to draw
+    year = str(mov.get_year())
+    direct = f'Directed By: {mov.get_director()}'
+    title = mov.get_name()
+    german_title = f'(dt: {mov.get_german_name()})'
+
+    # Load fonts
+    try:
+        font_year = ImageFont.truetype("arial.ttf", 100)
+        font_titles = ImageFont.truetype("arial.ttf", 50)
+        font_direct = ImageFont.truetype("arial.ttf", 40)
+    except IOError:
+        font_year = font_direct = font_titles = ImageFont.load_default()
+
+    # Define text positions (centered)
+    width, height = target_size
+    x_center = width // 2
+    y_title = height // 5
+    y_ger_title = height // 4 + 50
+    y_year = height // 2
+    y_direct = height * 3 // 4
+
+    # Draw wrapped text
+    draw_wrapped_text(draw, title, (x_center, y_title), font_titles)
+    if german_title != "(dt: -)":
+        draw_wrapped_text(draw, german_title, (x_center, y_ger_title), font_titles)
+    draw_wrapped_text(draw, year, (x_center, y_year), font_year)
+    draw_wrapped_text(draw, direct, (x_center, y_direct), font_direct)
+
+    # Save the image
+    filename = os.path.join(TARGET_BACK, f'{number}.png')
+    img.save(filename)
 
 
 def prepare_printing(movies: [Movie]):
